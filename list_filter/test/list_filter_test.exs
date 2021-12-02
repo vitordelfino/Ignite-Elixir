c794e240abe9880c7c0e9404c6648a23e2b477f3
Vitor Delfino <vitor.delfino@loft.com.br>
2021-12-02 09:40:31 -0300

    Desafio: Filtragem em lista -> ListFilter

--- list_filter/test/list_filter_test.exs
+++ list_filter/test/list_filter_test.exs
@@ -1,8 +1,18 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
+  describe "call/1" do
+    test "should return quantity of odd number" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+      response = ListFilter.call(list)
+      expected_response = 3
+      assert response == expected_response
+    end
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+    test "should return quantity of odd number when list is empty" do
+      list = []
+      response = ListFilter.call(list)
+      expected_response = 0
+      assert response == expected_response
+    end
   end
 end
